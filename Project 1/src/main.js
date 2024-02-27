beda5ab52497093bd5357a8d0cbe727e69b594b0
Conor Race <conorjat75@gmail.com>
2024-02-26 21:37:52 -0500

    Fixed favicon code and imported cookie demo code.

--- Project 1/src/main.js
+++ Project 1/src/main.js
@@ -326,4 +326,45 @@ const formErrorCheck = () => {
     return ret;
 };
 
+// Cookie Helper Code (implemented from localStorage/cookies in-class demo)
+function getCookieVal (offset) {
+	var endstr = document.cookie.indexOf (";", offset);
+	if (endstr == -1) { endstr = document.cookie.length; }
+	return unescape(document.cookie.substring(offset, endstr));
+	}
+
+function GetCookie (name) {
+	var arg = name + "=";
+	var alen = arg.length;
+	var clen = document.cookie.length;
+	var i = 0;
+	while (i < clen) {
+		var j = i + alen;
+		if (document.cookie.substring(i, j) == arg) {
+			return getCookieVal (j);
+			}
+		i = document.cookie.indexOf(" ", i) + 1;
+		if (i == 0) break; 
+		}
+	return null;
+	}
+
+function DeleteCookie (name,path,domain) {
+	if (GetCookie(name)) {
+		document.cookie = name + "=" +
+		((path) ? "; path=" + path : "") +
+		((domain) ? "; domain=" + domain : "") +
+		"; expires=Thu, 01-Jan-70 00:00:01 GMT";
+		}
+	}
+
+function SetCookie (name,value,maxAge,path,domain,sameSite,secure) {
+  document.cookie = name + "=" + escape (value) +
+    ((maxAge) ? ";max-age=" + maxAge  : "") +
+    ((path) ? ";path=" + path  : "") +
+    ((domain) ? ";domain=" + domain : "") +
+    ((sameSite) ? ";samesite=" + sameSite : ";samesite=strict") +
+    ((secure) ? ";secure;" : ";");
+}
+
 window.onload = init;
